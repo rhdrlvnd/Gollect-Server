a678e15bc006e08ba594c31f8172e84fc8bd128c
ChoeJiwon <jiwon0929@ajou.ac.kr>
2019-11-25 14:56:34 +0900

    add crowler

---
+++ src/routes/nonVideoCrowler/overwatchInven.js
@@ -0,0 +1,72 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+var mysql = require('mysql');
+var dbconfig = require('../../config/database.js');
+var connection = mysql.createConnection(dbconfig);
+const log = console.log;
+var RURL = "http://www.inven.co.kr/board/overwatch/4538"
+
+
+const getHtml = async (URL) => {
+  try {
+    return await axios.get(URL);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml(RURL)
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("#powerbbsBody tbody tr.ls").children("td.bbsSubject");
+    
+    $bodyList.each(function(i, elem) {
+      let targetUrl = $(this).find('a.sj_ln').attr('href');
+      ulList[i] = {
+          id:6,
+          title: $(this).find('a.sj_ln').text(),
+          url: targetUrl,
+      };
+    });
+    const data = ulList.filter(n => n.title);
+    return data;
+  })
+  .then(res =>{
+    res.forEach(function(data,i) {
+
+      getHtml(data.url).then(html=>{
+        
+        const $ = cheerio.load(html.data);
+        const $mText = $("#powerbbsContent");
+        const imgSrc = $mText.find('img').attr('src');
+        
+        const time = $("#tbArticle div.articleDate").text();
+        const text = $mText.text().substring(0,20);
+
+        return [text,time,imgSrc];
+      }).then(res => {
+        data.text = res[0];
+        data.time = res[1];
+        data.img = res[2];
+        return data;
+      }).then(res=>{
+		log(res)
+		let params = [res.id,res.title,res.text, res.url, res.img, res.time]
+        //log(res.id+','+res.title+','+res.text+','+res.url+','+res.img+','+res.time);
+        let sql = 'INSERT INTO textcontents (platform_id, title, abstract, url, img_src, uploaded_at) VALUES(?, ?, ?, ?, ?, ?)';
+        log("sql = "+sql);
+        
+        connection.query(sql, params, (err,rows,fields)=>{
+            if(err){
+                console.log(err.message);
+            }
+            else{
+                console.log('success!');
+            }
+        });
+
+        });
+      
+    });
+  });
