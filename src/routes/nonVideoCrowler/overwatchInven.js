814f03fb4eb95c5f0869daf831011b8813cb9aa1
ChoeJiwon <jiwon0929@ajou.ac.kr>
2019-12-07 18:28:47 +0900

    add domain_id

--- src/routes/nonVideoCrowler/overwatchInven.js
+++ src/routes/nonVideoCrowler/overwatchInven.js
@@ -28,6 +28,7 @@ function crowl(){
 			id:16,
 			title: $(this).find('a.sj_ln').text(),
 			url: targetUrl,
+			domain_id:5
 		};
 	  });
 	  const data = ulList.filter(n => n.title);
@@ -52,9 +53,9 @@ function crowl(){
 		  data.img = res[2];
 		  return data;
 		}).then(res=>{
-		  let params = [res.id,res.title,res.text, res.url, res.img, res.time]
+		  let params = [res.id,res.title,res.text, res.url, res.img, res.time,res.domain_id]
 		  //log(res.id+','+res.title+','+res.text+','+res.url+','+res.img+','+res.time);
-		  let sql = 'INSERT INTO textcontents (platform_id, title, abstract, url, img_src, uploaded_at) VALUES(?, ?, ?, ?, ?, ?)';
+		  let sql = 'INSERT INTO textcontents (platform_id, title, abstract, url, img_src, uploaded_at, domain_id) VALUES(?, ?, ?, ?, ?, ?, ?)';
 		  
 		  connection.query(sql, params, (err,rows,fields)=>{
 			  if(err){
