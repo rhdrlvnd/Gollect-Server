1dbd24693321a03b234edb9cb9875bc1cb25d459
Dawoon <rhdrlvnd@ajou.ac.kr>
2020-01-08 17:27:36 +0900

    update index.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-print("진행중");
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contents', contentsRouter);
